f86fe472b359c44b335390c84585c96d92f209bf
Aditya-138-12 <adityasaroha456@gmail.com>
2025-04-03 11:18:41 +0530

    Added onValue on redirect.js

--- src/components/redirecting_algo/redirecting.js
+++ src/components/redirecting_algo/redirecting.js
@@ -1,15 +1,26 @@
 import {React, useEffect} from "react";
 import { useLocation, Navigate, useNavigate } from "react-router-dom";
+import { db } from "../firebaseConfig/firebaseConfig";
+import {ref, set, push, onValue} from 'firebase/database';
 
 const Redirect = () => {
 
     const location = useLocation();
     const navigate = useNavigate();
 
-    console.log(location.pathname.slice(1));
+    //console.log(location.pathname.slice(1));
+
+    let shortLink = location.pathname.slice(1)
     
     useEffect(()=>{
-        navigate('//youtube.com');
+        const dbRef = ref(db, "shortURLs/");
+
+        onValue(dbRef, (snapshot)=>{
+            const data = snapshot.val();
+            console.log(Object.values(data));
+        })
+
+        //navigate('//youtube.com');
     }, []);
 
     return (
